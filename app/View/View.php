ff25d3587c8c90c6ce07f77861795c02ec20f457
Phil Kurth <phil@philkurth.com.au>
2019-04-15 12:44:14 +1000

    examples

--- app/View/View.php
+++ app/View/View.php
@@ -16,8 +16,19 @@ class View extends \DsvgIcons\Framework\View\ViewBase {
 	protected function setup() {
 		$this->set_view_base_dir( DSVGI_PLUGIN_DIR . 'templates' );
 		$this->set_view_override_base_dir( get_stylesheet_directory() . '/dsvgicons' );
-//		$this->add_overridable_template_dir( 'icons' );
-//		$this->add_overridable_template( 'icons/umbrella.php' );
+
+		// make all templates overridable
+		$this->make_all_templates_overridable();
+
+		// OR, specify which directories contain overridable templates
+		$this->set_overridable_template_dirs( [ 'dir', 'some/dir' ] );
+		$this->add_overridable_template_dir( 'icons' );
+		$this->add_overridable_template_dir( 'widgets' );
+
+		// AND/OR, mark specific templates as overridable
+		$this->set_overridable_templates( [ 'icons/umbrella', 'icons/tophat' ] );
+		$this->add_overridable_template( 'icons/umbrella' );
+		$this->add_overridable_template( 'icons/tophat' );
 	}
 
 
