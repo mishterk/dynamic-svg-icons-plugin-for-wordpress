5bf338c1051dc608610ada25ead16dc6d4229cdf
Phil Kurth <phil@philkurth.com.au>
2019-04-16 16:28:38 +1000

    set version 1.0

--- dynamic-svg-icons.php
+++ dynamic-svg-icons.php
@@ -3,7 +3,7 @@
  * Plugin Name: Dynamic SVG Icons
  * Plugin URI:  TODO
  * Description: Dynamically place SVG icons and images on your site using placeholder markup.
- * Version:     0.1
+ * Version:     1.0
  * Author:      Phil Kurth
  * Author URI:  http://philkurth.com.au
  * License:     GPL2
@@ -16,11 +16,17 @@
 // If this file is called directly, abort.
 defined( 'WPINC' ) or die();
 
-define( 'DSVGI_PLUGIN_VERSION', 0.1 );
+define( 'DSVGI_MIN_PHP_VERSION', '7' );
+define( 'DSVGI_PLUGIN_VERSION', 1.0 );
 define( 'DSVGI_PLUGIN_TEXT_DOMAIN', 'dynamic-svg-icons' );
 define( 'DSVGI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DSVGI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-require_once DSVGI_PLUGIN_DIR . 'app/DynamicSvgIconsPlugin.php';
-$plugin = new \DynamicSvgIcons\DynamicSvgIconsPlugin();
-$plugin->init();
+if ( version_compare( PHP_VERSION, DSVGI_MIN_PHP_VERSION, '>=' ) ) {
+	require_once DSVGI_PLUGIN_DIR . 'app/DynamicSvgIconsPlugin.php';
+	$plugin = new \DynamicSvgIcons\DynamicSvgIconsPlugin();
+	$plugin->init();
+
+} else {
+	add_action( 'admin_notices', 'envato_partner_plugin_php_version_fail_notice' );
+}
